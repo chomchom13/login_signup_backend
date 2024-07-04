7658804b7de51d36c3affb0ef5f0dd7e4e60a5cf
namannc13 <chawhannaman5@gmail.com>
2024-07-04 11:53:15 +0530

    commit

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import "./config/db.js";
 import { authRouter } from "./routes/auth.route.js";
+const productRoutes = require('./routes/product');
+const cartRoutes = require('./routes/cart');
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api", authRouter);
+app.use('/api/products', productRoutes);
+app.use('/api/cart', cartRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
